81ea252545d5260898ecbfa092ac7e8379dcd35f
wcoding <mailbox@wcoding.ru>
2015-01-22 18:00:35 +0600

    Первый коммит

---
+++ theme/v_article.php
@@ -0,0 +1,8 @@
+<?/*
+Шаблон просмотра статьи
+=======================
+$article['title'] - заголовок
+$article['content'] - содержание
+*/?>
+<h1><?=$article['title']?></h1>
+<div><?=$article['content']?></div>
