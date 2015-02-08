7f7201a9c2545a84ed01a05851a7194b52666125
wcoding <mailbox@wcoding.ru>
2015-02-08 17:17:16 +0600

    Реализованы методы редактирования и вывода списка пользователей

--- application/classes/controller/admin/C_UsersEditor.php
+++ application/classes/controller/admin/C_UsersEditor.php
@@ -6,7 +6,8 @@
 class C_UsersEditor extends C_BaseAdmin
 {
 
-    protected function before(){
+    protected function before()
+    {
 
         parent::before();
 
@@ -22,12 +23,64 @@ class C_UsersEditor extends C_BaseAdmin
     /**
      * Экшн главной страницы редактора пользователей.
      */
-    public function action_index(){
+    public function action_index()
+    {
 
         // Название страницы
         $this->title .= ' :: Редактор пользователей';
 
         // Подготовить внутренний шаблон страницы для передачи его в базовый шаблон
-        $this->content = $this->GetHtml( 'v_users_editor.php' );
+        $this->content = $this->GetHtml( 'v_users_editor.php',
+            array(
+                'users' => $this->mUsers->GetAll(),
+            )
+        );
+    }
+
+
+
+    public function action_edit()
+    {
+        // Нажали кнопку, хотим изменить данные
+        if($this->IsPost()){
+
+            // Если валидация полей формы прошла успешно и данные сохранены
+            if( FALSE !== $this->mUsers->Edit($_POST['userID'], $_POST['username'], $_POST['role']) ){
+
+                header("Location: index.php?c=UsersEditor&act=edit&id={$_POST['userID']}");
+                exit;
+            }
+            else{
+                $message = 'Все поля должны быть заполнены.';
+                $user = $this->mUsers->Get($_GET['id']);
+            }
+        }
+        // Только пришли
+        elseif( isset($_GET['id']) and $_GET['id'] != '' ){
+
+            // Запросить в базе, данные пользователя
+            $user = $this->mUsers->Get($_GET['id']);
+
+            // Если был передан id несуществующего пользователя
+            if( 1 > count($this->mUsers->Get($_GET['id'])) ){
+                $this->NotFound();
+            }
+        }
+        // Если id пользователя не указан
+        else{
+            $this->NotFound();
+        }
+
+        // Название страницы
+        $this->title .= ' :: Редактировать данные';
+
+        // Подготовить внутренний шаблон страницы для передачи его в базовый шаблон
+        $this->content = $this->GetHtml( 'v_users_edit.php',
+            array(
+                'user' => $user, // массив с данными пользователя
+                'roles' => $this->mUsers->GetRoles(), // список всех ролей
+                'message' => $message, // сообщение валидации полей формы
+            )
+        );
     }
 }
