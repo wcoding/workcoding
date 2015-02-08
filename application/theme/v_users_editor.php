db64b4aa4e0517601f83d14bf8122fc5b591e95e
wcoding <mailbox@wcoding.ru>
2015-02-08 17:16:29 +0600

    Создан шаблон главной страницы - редактора пользователей

--- application/theme/v_users_editor.php
+++ application/theme/v_users_editor.php
@@ -1,6 +1,26 @@
 <?/*
 Шаблон редактора пользователей
 ==============================
-
+$user['id_user']      идентификатор пльзователя
+$user['username']     ник пользователя
+$user['login']        логин
+$user['description']  описание роли пользователя
 */?>
-<h3>Welcome!</h3>
+
+<table class="users-editor">
+    <thead>
+        <tr>
+            <th>Имя пользователя
+            <th>E-mail(login)
+            <th>Роль
+    </thead>
+    <tbody>
+<? foreach($users as $user): ?>
+    <tr>
+        <td><a href="index.php?c=UsersEditor&act=edit&id=<?=$user['id_user']?>"><?=$user['username']?></a></td>
+        <td><?=$user['login']?></td>
+        <td><?=$user['description']?></td>
+    </tr>
+<? endforeach; ?>
+    </tbody>
+</table>
