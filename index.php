e50691db7c65076f61a6f69df321ecf00f9acb6d
wcoding <mailbox@wcoding.ru>
2015-02-09 11:44:52 +0600

    Формирование массива params[] из URL. В swith-case изменил default: редирект на 404.

--- index.php
+++ index.php
@@ -13,11 +13,32 @@ require APPPATH . 'startup.php';
 // Префикс методов класса, из конфига
 $action = Core::GetConfig('settings', 'actionPrefix');
 
+$info = explode('/', $_GET['q']);
+$params = array();
+
+for ($i=0; $i <= count($info); $i++)
+{
+	if ($info[$i] != ''){
+
+		if($i == 0){
+			$params['controller'] = $info[$i];
+			continue;
+		}
+
+		if($i == 1){
+			$params['action'] = $info[$i];
+			continue;
+		}
+
+		$params[] = $info[$i];
+	}
+}
+
 // Получить название метода класса из URL
-$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';
+$action .= isset($params['action']) ? $params['action'] : 'index';
 
 // Получить название контроллера из URL
-$c = isset($_GET['c']) ? $_GET['c'] : '' ;
+$c = isset($params['controller']) ? $params['controller'] : '' ;
 
 // Создать экземпляр класса.
 // При попытке создать экземпляр класса
@@ -40,19 +61,28 @@ switch ($c)
 	case 'PagesEditor':
 		$controller = new C_PagesEditor;
 		break;
+	case 'auth':
+		$controller = new C_Auth;
+		break;
 	case 'profile':
 		$controller = new C_UserPanel;
+		break;
+	case 'article':
+		$controller = new C_Article;
 		break;
 	case 'page':
 		$controller = new C_Pages;
 		break;
-	case 'auth':
-		$controller = new C_Auth;
+	case '':
+		$controller = new C_Index;
 		break;
 	default:
-		$controller = new C_Articles;
+		// От бесконечных дублей главной страницы
+		header("HTTP/1.0 404 Not Found");
+		include(VIEW.'404.php');
+		exit;
 }
 
 // Передать имя метода, который должен отработать у класса
 // экземпляр которого только что был создан.
-$controller->Request($action);
+$controller->Request($action, $params);
