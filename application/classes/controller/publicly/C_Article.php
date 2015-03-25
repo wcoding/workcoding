aeb1c9e9efad8475cb1062ca06815f0ab9d7da2d
wcoding <mailbox@wcoding.ru>
2015-03-25 20:01:11 +0600

    bug redefinition parent class constructor

--- application/classes/controller/publicly/C_Article.php
+++ application/classes/controller/publicly/C_Article.php
@@ -6,10 +6,15 @@ class C_Article extends C_BasePublicly
 {
     private $mArticle;// экземпляр класса модели статей
     private $mComments;// экземпляр класса модели комментариев к статье
+    
 
-
-    function __construct()
+    /**
+     * Метод подготавливает данные, которые будут использоваться
+     * в методе action_*
+     */
+    protected function before()
     {
+        parent::before();
         $this->mArticle = M_Article::Instance();
         $this->mComments = M_Comment::Instance();
     }
@@ -21,20 +26,19 @@ class C_Article extends C_BasePublicly
     public function action_index()
     {
         // Если параметры переданы через URL
-        if(isset($this->params[0])){
+        if(isset($this->params[0])) {
 
             // Извлечение статьи.
             $article = $this->mArticle->Get($this->params[0]);
 
             // если в базе нет такой статьи
-            if(!is_array($article)){
+            if(!is_array($article)) {
                 $this->NotFound();
             }
 
             // Получить комментарии к статье
             $comments = $this->mComments->Get($this->params[0]);
-        }
-        else{
+        } else {
             $this->NotFound();
         }
 
@@ -50,7 +54,8 @@ class C_Article extends C_BasePublicly
             array(
                 'article' => $article, // ассоциативный массив выбраной статьи
                 'comments' => $comments, // ассоциативный массив комментариев к статье
-                'error' => $error // флаг ошибки при комментировании
+                'error' => $error, // флаг ошибки при комментировании
+                'user' => $this->mUsers->Get()
             )
         );
     }
@@ -62,15 +67,18 @@ class C_Article extends C_BasePublicly
     public function action_comments()
     {
 
-        if($this->IsPost()){
-
-            // если новый комментарий сохранён
-            if ( $this->mComments->Add($_POST['id'], $_POST['name'], $_POST['message']) )
+        if($this->IsPost() and null !== $this->mUsers->Get()) {
+            
+            if ( $this->mComments->Add($_POST['id'],
+                 $this->mUsers->Get()['id_user'],
+                 Core::TextOnly($_POST['message'])) ) {
+                
+                // если новый комментарий сохранён
                 $this->Redirect('/article/index/'.$_POST['id']);
-
-            // если не все поля были заполнены
-            else
+                
+            } else {
                 $this->Redirect('/article/index/'.$_POST['id'].'/error#comments');
+            }
         }
 
         // если обратились к экшену напрямую
